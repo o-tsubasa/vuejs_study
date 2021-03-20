e1f0cb3e0109cbdd9fbb9b45fc8e53f9e0be45ba
tsubasa <tsubasa@tsubasanoMacBook-Air.local>
2021-03-20 11:10:38 +0900

    transition-group

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="main">
     <button @click="show = !show">切り替え</button>
-
+    <br>
+        <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <transition-group name="fade">
+        <li
+          style="cursor: pointer;"
+          v-for="(number, index) in numbers"
+          :key="number"
+          @click="remove(index)"
+        >{{ number }}</li>
+      </transition-group>
+    </ul>
     <div>
       <button @click="myComponent = 'ComponentA'">ComponentA</button>
       <button @click="myComponent = 'ComponentB'">ComponentB</button>
@@ -62,12 +73,24 @@ export default {
   },
   data() {
     return {
+      numbers: [0, 1, 2],
+      nextNumber: 3,
       show: true,
       myAnimation: 'fade',
       myComponent: "ComponentA"
     };
   },
   methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    },
     beforeEnter(el) {
       // 現れる前
       el.style.transform = 'scale(0)'
@@ -124,13 +147,16 @@ export default {
   background-color: deeppink;
   border-radius: 100px;
 }
+.fade-move {
+  transition: transform 1s;
+}
 .fade-enter {
   /* 現れる時の最初の状態 */
   opacity: 0;
 }
 .fade-enter-active {
   /* 現れる時のトランジションの状態 */
-  transition: opacity .5s;
+  transition: opacity 1s;
 }
 .fade-enter-to {
   /* 現れる時の最後の状態 */
@@ -142,7 +168,9 @@ export default {
 }
 .fade-leave-active {
   /* 消える時のトランジションの状態 */
-  transition: opacity .5s;
+  transition: opacity 1s;
+  position: absolute;
+  width: 200px;
 }
 .fade-leave-to {
   /* 消える時の最後の状態 */

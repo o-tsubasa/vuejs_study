48019b35ef4516a688bf783f1031d54a4b77d1b7
tsubasa <tsubasa@tsubasanoMacBook-Air.local>
2021-03-21 21:51:54 +0900

    Vue Router　導入前

---
+++ src/_old/4/App.vue
@@ -0,0 +1,207 @@
+<template>
+  <div class="main">
+    <button @click="show = !show">切り替え</button>
+    <br>
+        <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <transition-group name="fade">
+        <li
+          style="cursor: pointer;"
+          v-for="(number, index) in numbers"
+          :key="number"
+          @click="remove(index)"
+        >{{ number }}</li>
+      </transition-group>
+    </ul>
+    <div>
+      <button @click="myComponent = 'ComponentA'">ComponentA</button>
+      <button @click="myComponent = 'ComponentB'">ComponentB</button>
+    </div>
+    <transition name="fade" mode="out-in">
+      <component :is="myComponent"></component>
+    </transition>
+
+    <transition name="fade" mode="out-in">
+      <p v-if="show" key="bye">さようなら</p>
+      <p v-if="!show" key="hello">こんにちは</p>
+    </transition>
+
+    <transition name="fade" appear>
+      <p v-if="show">hello</p>
+    </transition>
+    <transition
+      enter-active-class="animate__animated animate__bounce"
+      leave-active-class="animate__animated animate__flash"
+      appear>
+      <p v-if="show">hello2</p>
+    </transition>
+    <transition name="slide" type="animation" appear>
+      <p v-if="show">bye</p>
+    </transition>
+
+    <button @click="myAnimation = 'slide'">Slide</button>
+    <button @click="myAnimation = 'fade'">Fade</button>
+    <p>{{ myAnimation }}</p>
+    <transition :name="myAnimation" appear>
+      <p v-if="show">bye</p>
+    </transition>
+
+    <!-- @after-enter="afterEnter"
+    @enter-cancelled="enterCancelled"
+    @after-leave="afterLeave"
+    @before-leave="beforeLeave"
+    @leave-cancelled="leaveCancelled" -->
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave"
+    >
+      <div class="circle" v-if="show"></div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
+
+export default {
+  components: {
+    ComponentA,
+    ComponentB
+  },
+  data() {
+    return {
+      numbers: [0, 1, 2],
+      nextNumber: 3,
+      show: true,
+      myAnimation: 'fade',
+      myComponent: "ComponentA"
+    };
+  },
+  methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el) {
+      // 現れる前
+      el.style.transform = 'scale(0)'
+    },
+    enter(el, done) {
+      // 現れるとき
+      // done() // アニメーションが終わったことを教える関数
+      let scale = 0
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1
+        if (scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    // afterEnter() {
+    //   // 現れた後
+    // },
+    // enterCancelled() {
+    //   // 現れるアニメーションがキャンセルされたとき
+    // },
+    // beforeLeave() {
+    //   // 消える前
+    // },
+    leave(el, done) {
+      // 消えるとき
+      let scale = 1
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1
+        if (scale < 0) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    // afterLeave() {
+    //   // 消えた後
+    // },
+    leaveCancelled() {
+      // 消えるアニメーションがキャンセルされたとき
+    },
+  }
+}
+</script>
+
+<style scoped>
+.circle {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  background-color: deeppink;
+  border-radius: 100px;
+}
+.fade-move {
+  transition: transform 1s;
+}
+.fade-enter {
+  /* 現れる時の最初の状態 */
+  opacity: 0;
+}
+.fade-enter-active {
+  /* 現れる時のトランジションの状態 */
+  transition: opacity 1s;
+}
+.fade-enter-to {
+  /* 現れる時の最後の状態 */
+  opacity: 1;
+}
+.fade-leave {
+  /* 消える時の最初の状態 */
+  opacity: 1;
+}
+.fade-leave-active {
+  /* 消える時のトランジションの状態 */
+  transition: opacity 1s;
+  position: absolute;
+  width: 200px;
+}
+.fade-leave-to {
+  /* 消える時の最後の状態 */
+  opacity: 0;
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-active {
+  animation: slide-in .5s;
+  transition: opacity 1s;
+}
+.slide-leave-active {
+  animation: slide-in .5s reverse;
+  transition: opacity 1s;
+}
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,55 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
-// import LikeNumber from './components/LikeNumber.vue';
 
 Vue.config.productionTip = false
-
-// コンポーネントをグローバル登録する場合
-// Vue.component('LikeNumber', LikeNumber);
-
-// Vue.directive("border", {
-//   bind(el, binding, vnode) {
-//     // ディレクティブが初めてぶｔ対象の要素に紐付いたとき（1回のみ）
-//   },
-//   // inserted(el, binding, vnode) {
-//   //   // 親Nodeに挿入されたとき（mountedみたいなイメージ）
-//   // },
-//   update(el, binding, vnode, oldVnode) {
-//     // コンポーネントが更新されるたび。子コンポーネントのVNodeが更新される前。
-//   },
-//   // componentUpdated(el, binding, vnode, oldVnode) {
-//   //   // コンポーネントが更新されるたび。子コンポーネントのVNodeが更新された後。
-//   // },
-//   // unbind(el, binding, vnode) {
-//   //   // ディレクティブが紐付いている要素から取り除かれたとき（destroyedみたいなイメージ）
-//   // },
-// });
-
-// グローバルにディレクティブを追加
-// 第2引数に渡した関数はbindとupdateに適用される
-// el: カスタムディレクティブが紐づく要素
-// Vue.directive("border", function(el, binding) {
-//   el.style.borderWidth = binding.value.width;
-//   el.style.borderColor = binding.value.color;
-//   el.style.borderStyle = binding.arg;
-//   if (binding.modifiers.round) {
-//     el.style.borderRadius = "0.5rem";
-//   }
-//   if (binding.modifiers.shadow) {
-//     el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
-//   }
-// });
-
-// 関数の引数は必ず取る
-Vue.filter("upperCase", function(value) {
-  return value.toUpperCase();
-});
-
-Vue.mixin({
-  created() {
-    console.log("global mixin");
-  }
-})
 
 new Vue({
   // ES6の書き方
